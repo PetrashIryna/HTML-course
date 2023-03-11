6d0d68e415270d51de68b7b51b2cfec94f027ed0
PetrashIryna <malvina67@ukr.net>
2023-03-11 09:40:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    // autoWidth: true,
+    // autoplay: true,
+    // autoplayTimeout: 1000,
+    // autoplayHoverPause: true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            // items:5,
+            items:2,
+            nav:true,
+            navText: ['hello', 'world']
+            // loop:false
+        }
+    }
+})
+});
